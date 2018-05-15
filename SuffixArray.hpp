891c0ff6113c8021f8f1328bd82e4c18ed475806
bravewarrior0 <bravewarrior1997@gmail.com>
2018-05-15 15:34:04 +0000

    Adding suffix array

---
+++ SuffixArray.hpp
@@ -0,0 +1,32 @@
+#include <cstring>
+#include <algorithm>
+
+struct suffix
+{
+    int index;
+    char *suff;
+};
+
+bool cmp(struct suffix a, struct suffix b)
+{
+    return strcmp(a.suff, b.suff) < 0;
+}
+
+int *buildSuffixArray(char *txt, int size)
+{
+    suffix suffixes[size];
+    for (int i = 0; i < size; i++)
+    {
+        suffixes[i].index = i;
+        suffixes[i].suff = txt + i;
+    }
+
+    std::sort(suffixes, suffixes + size, cmp);
+
+    int *suffixArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        suffixArray[i] = suffixes[i].index;
+    }
+    return suffixArray;
+}
